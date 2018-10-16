3ef01f9f13022f2f36fa44f437a4f064e0a47abf
ahance21 <42975655+ahance21@users.noreply.github.com>
2018-10-16 12:22:27 -0400

    Add files via upload

---
+++ src/MadLib.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class MadLib
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner keyboard = new Scanner(System.in);
+		System.out.print("Please enter your name:  ");
+		String name = keyboard.nextLine();
+		System.out.print("\nPlease enter a Verb: ");
+		String verb = keyboard.nextLine();
+		System.out.print("\nPlease enter a Noun:  ");
+		String noun = keyboard.nextLine();
+		System.out.print("\nPlease enter a Noun:  ");
+		String noun2 = keyboard.nextLine();
+		System.out.print("\nPlease enter a Plural Noun:  ");
+		String pluralnoun = keyboard.nextLine();
+		System.out.print("\nPlease enter an Adjective:  ");
+		String adjective = keyboard.nextLine();
+		System.out.print("\nPlease enter an Adjective:  ");
+		String adj2 = keyboard.nextLine();
+		System.out.print("\nPlease enter a noun:  ");
+		String noun3 = keyboard.nextLine();
+		System.out.print("\nPlease enter a verb:  ");
+		String verb2 = keyboard.nextLine();
+		System.out.print("\nPlease enter a Plural Noun:  ");
+		String pluralnoun2 = keyboard.nextLine();
+		System.out.print("\nPlease enter a noun:  ");
+		String noun4 = keyboard.nextLine();
+		
+		
+		
+		System.out.println("The quick " +  verb  + "  " +  noun  + " Flew on the " +  noun2  + " on the way to holland. \nWhen " +  noun  + " landed, he was greeted by many " +  pluralnoun  + 
+				" and he headed torwards the " +  adjective + "  " +  adj2  +  "  " + noun3  + " The workers all " +  verb2 + "  "  + " kicked him out and banned him, After this, he was swarmed by a bunch of "
+				 +  pluralnoun2  + " and was taken away in the " +  noun  + " and was never seen again ");
+		
+		 
+		
+		
+				
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
